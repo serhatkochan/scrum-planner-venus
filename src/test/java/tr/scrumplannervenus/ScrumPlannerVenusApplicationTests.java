698d9928b063e11564742d1890adbbbd559e8fd9
Serhat <serhatkochan@hotmail.com.tr>
2023-10-09 09:11:06 +0300

    SpringBootTest

--- src/test/java/tr/scrumplannervenus/ScrumPlannerVenusApplicationTests.java
+++ src/test/java/tr/scrumplannervenus/ScrumPlannerVenusApplicationTests.java
@@ -3,7 +3,7 @@ package tr.scrumplannervenus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class ScrumPlannerVenusApplicationTests {
 
 	@Test
